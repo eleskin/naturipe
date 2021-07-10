8f0a0b35f7d8cb715906810207a20f002f3df3d1
aleksandreleskin <eleskin1201@gmail.com>
2021-07-11 00:20:39 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app" :style="{ backgroundImage: 'url(' + backgroundImage + ')'}">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import backgroundImage from './assets/img/background-image.jpg';
+export default {
+  name: 'App',
+  data: () => ({
+    backgroundImage: backgroundImage
+  }),
+};
+</script>
+
+<style>
+.app {
+  background: no-repeat center center;
+  background-size: cover;
+  min-height: 100vh;
+  width: 100vw;
+}
+</style>
